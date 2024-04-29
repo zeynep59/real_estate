72ed794abe2c6d89e2de8e732d467d63b6e5afbd
EdaaKurtoglu <120361946+EdaaKurtoglu@users.noreply.github.com>
2024-04-29 22:57:00 +0300

    Stepper/Özellikler

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:real_estate/screens/signin.dart';
 import 'package:real_estate/screens/signup.dart';
 import 'package:real_estate/screens/form_page.dart';
+import 'package:real_estate/screens/stepper_formPage.dart';
 import 'package:real_estate/screens/welcome.dart';
 import 'package:real_estate/theme/theme.dart';
+import 'package:real_estate/screens/home_screen.dart';
+import 'package:real_estate/widgets/additionalFeatures.dart';
+import 'package:real_estate/widgets/estate_features.dart';
+import 'package:real_estate/widgets/opportunities.dart';
+import 'package:real_estate/screens/stepper_formPage.dart';
+import 'package:real_estate/widgets/select_category.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +26,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: lightMode,
-      home: const WelcomePage(),
+      //home: const WelcomePage(),
+      home: StepperPage(),
       routes: {
         '/sign_in': (context) => SignInScreen(),
         '/sign_up': (context) => SignUpScreen(),
