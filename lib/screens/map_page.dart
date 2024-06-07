3ea47e6e5964444c8ce98cdc6fb8a2fffd25ea4c
Ozan Yiğit <ozanyigittyilmaz@gmail.com>
2024-06-07 08:42:46 +0300

    mappage search eklendi

--- lib/screens/map_page.dart
+++ lib/screens/map_page.dart
@@ -3,19 +3,17 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:location/location.dart'
-    as loc; // Use alias 'loc' for the 'location' package
+import 'package:location/location.dart' as loc;
 import 'package:real_estate/screens/stepper_formPage.dart';
 import 'package:real_estate/widgets/search_field.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:real_estate/screens/professionels.dart';
-import 'package:geocoding/geocoding.dart'; // Geocoding package for reverse geocoding
+import 'package:geocoding/geocoding.dart';
 import 'package:real_estate/services/database_service.dart';
 import 'package:real_estate/screens/settings.dart';
 import '../components/searchField.dart';
 import '../models/address.dart';
 import '../models/house.dart';
-import '../screens/FavoritesPanel.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -25,19 +23,18 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  loc.Location _locationController =
-      loc.Location(); // Use alias 'loc' for the 'location' package
+  loc.Location _locationController = loc.Location();
   late GoogleMapController _mapController;
-  LatLng _currentLocation = const LatLng(37.4223, -122.0848);
+  LatLng _currentLocation = const LatLng(41.0082, 28.9784); // Istanbul's coordinates
   String _currentCity = "Loading...";
   final currentUser = FirebaseAuth.instance.currentUser!;
   bool _showFavorites = false;
-  int _selectedIndex = 0;
 
   final cityController = TextEditingController();
   final countryController = TextEditingController();
   final districtController = TextEditingController();
   final streetController = TextEditingController();
+  final searchController = TextEditingController(); // Controller for the search field
 
   TextStyle labelTextStyle = const TextStyle(
     fontSize: 16,
@@ -46,17 +43,15 @@ class _MapPageState extends State<MapPage> {
     fontFamily: 'Arial',
   );
 
-  // Database Service instance
   final DatabaseService _databaseService = DatabaseService();
 
   @override
   void initState() {
     super.initState();
     _getCurrentLocation();
-    _getCurrentUser(); // Call a method to get current user details
+    _getCurrentUser();
   }
 
-  // Method to get current user details
   void _getCurrentUser() {
     final currentUser = FirebaseAuth.instance.currentUser;
     if (currentUser != null) {
@@ -66,7 +61,6 @@ class _MapPageState extends State<MapPage> {
     }
   }
 
-  // Method to get the current location and reverse geocode it
   Future<void> _getCurrentLocation() async {
     bool _serviceEnabled;
     loc.PermissionStatus _permissionGranted;
@@ -87,10 +81,8 @@ class _MapPageState extends State<MapPage> {
       }
     }
 
-    final loc.LocationData _locationData =
-        await _locationController.getLocation();
-    final LatLng currentLatLng =
-        LatLng(_locationData.latitude!, _locationData.longitude!);
+    final loc.LocationData _locationData = await _locationController.getLocation();
+    final LatLng currentLatLng = LatLng(_locationData.latitude!, _locationData.longitude!);
 
     setState(() {
       _currentLocation = currentLatLng;
@@ -99,11 +91,9 @@ class _MapPageState extends State<MapPage> {
     _reverseGeocodeLatLng(currentLatLng);
   }
 
-  // Method to reverse geocode a LatLng position
   Future<void> _reverseGeocodeLatLng(LatLng latLng) async {
     try {
-      List<Placemark> placemarks =
-          await placemarkFromCoordinates(latLng.latitude, latLng.longitude);
+      List<Placemark> placemarks = await placemarkFromCoordinates(latLng.latitude, latLng.longitude);
       if (placemarks.isNotEmpty) {
         Placemark place = placemarks[0];
         setState(() {
@@ -119,6 +109,23 @@ class _MapPageState extends State<MapPage> {
       }
     } catch (e) {
       print(e);
+    }
+  }
+
+  Future<void> _searchAndNavigate() async {
+    try {
+      List<Location> locations = await locationFromAddress(searchController.text);
+      if (locations.isNotEmpty) {
+        Location location = locations.first;
+        LatLng target = LatLng(location.latitude, location.longitude);
+        setState(() {
+          _currentLocation = target;
+        });
+        _mapController.animateCamera(CameraUpdate.newLatLngZoom(target, 13));
+        _reverseGeocodeLatLng(target);
+      }
+    } catch (e) {
+      print('Error occurred while searching: $e');
     }
   }
 
@@ -145,10 +152,24 @@ class _MapPageState extends State<MapPage> {
             ),
             const SizedBox(
               width: 10,
-            ), // Add some space between location and search field
+            ),
             Expanded(
-              child:
-                  SearchFieldWithSelection(), // Use the custom SearchFieldWithSelection widget
+              child: TextField(
+                controller: searchController,
+                decoration: InputDecoration(
+                  hintText: 'Search location',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: _searchAndNavigate,
+                  ),
+                ),
+                onSubmitted: (value) {
+                  _searchAndNavigate();
+                },
+              ),
             ),
           ],
         ),
@@ -156,8 +177,7 @@ class _MapPageState extends State<MapPage> {
       body: Stack(
         children: [
           GoogleMap(
-            initialCameraPosition:
-                CameraPosition(target: _currentLocation, zoom: 13),
+            initialCameraPosition: CameraPosition(target: _currentLocation, zoom: 13),
             onMapCreated: (GoogleMapController controller) {
               _mapController = controller;
             },
@@ -176,12 +196,10 @@ class _MapPageState extends State<MapPage> {
             },
           ),
           SlidingUpPanel(
-            panel:
-                _showFavorites ? _buildFavoritesPanel() : _buildAddressPanel(),
+            panel: _showFavorites ? _buildFavoritesPanel() : _buildAddressPanel(),
             onPanelClosed: () {
               setState(() {
-                _showFavorites =
-                    false; // Ensure the flag is reset when the panel is closed
+                _showFavorites = false;
               });
             },
           ),
@@ -194,39 +212,37 @@ class _MapPageState extends State<MapPage> {
         padding: const EdgeInsets.all(16),
         tabBackgroundColor: Colors.grey.shade800,
         activeColor: Colors.white,
-        selectedIndex: _selectedIndex, // Set the selected index
-        onTabChange: (index) {
-          setState(() {
-            _selectedIndex = index; // Update the selected index
-          });
-          if (index == 0) {
-            Navigator.pushNamed(context, '/map_page');
-          } else if (index == 1) {
-            Navigator.pushNamed(context, '/professionels');
-          } else if (index == 2) {
-            Navigator.pushNamed(context, '/history');
-          } else if (index == 3) {
-            Navigator.pushNamed(context, '/settings');
-          }
-        },
         tabs: const [
           GButton(
             icon: Icons.home,
             text: "Home",
           ),
           GButton(
-            icon: Icons.people_outline_rounded,
-            text: "Professionals",
+            icon: Icons.search,
+            text: "Search",
           ),
           GButton(
-            icon: Icons.history,
-            text: "History",
+            icon: Icons.favorite_border,
+            text: "Favorites",
           ),
           GButton(
             icon: Icons.settings,
             text: "Settings",
           ),
         ],
+        onTabChange: (index) {
+          setState(() {
+            if (index == 0) {
+              _showFavorites = false;
+            } else if (index == 1) {
+              Navigator.pushNamed(context, '/professionels');
+            } else if (index == 2) {
+              _showFavorites = true;
+            } else if (index == 3) {
+              Navigator.pushNamed(context, '/settings');
+            }
+          });
+        },
       ),
     );
   }
@@ -240,9 +256,7 @@ class _MapPageState extends State<MapPage> {
         } else if (snapshot.hasError) {
           return Text('Error: ${snapshot.error}');
         } else {
-          List<House> houses =
-              snapshot.data!.docs.map((doc) => doc.data()).toList();
-
+          List<House> houses = snapshot.data!.docs.map((doc) => doc.data()).toList();
           return ListView.builder(
             itemCount: houses.length,
             itemBuilder: (context, index) {
@@ -250,34 +264,26 @@ class _MapPageState extends State<MapPage> {
               return Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8.0),
                 child: OutlinedButton(
-                  // Buton şeklinde düğme
-                  onPressed: () {
-                    // Butona basıldığında yapılacak işlemler buraya gelecek
-                  },
+                  onPressed: () {},
                   style: OutlinedButton.styleFrom(
-                    backgroundColor:
-                        Colors.grey[200], // Butonun arka plan rengi
+                    backgroundColor: Colors.grey[200],
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(
-                          8.0), // Butonun kenarlarını yuvarlayalım
+                      borderRadius: BorderRadius.circular(8.0),
                     ),
                   ),
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Column(
-                      // Satır yerine sütun kullanarak alt alta yazıları yerleştirelim
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Row(
                           children: [
-                            Icon(Icons.home), // Ev ikonu
-                            SizedBox(width: 8), // Araya biraz boşluk ekleyelim
+                            Icon(Icons.home),
+                            SizedBox(width: 8),
                             Text('Price: ${house.price}'),
                           ],
                         ),
-                        SizedBox(
-                            height:
-                                8), // Yazılar arasına biraz boşluk ekleyelim
+                        SizedBox(height: 8),
                         Text('Location: ${house.address.city}'),
                       ],
                     ),
@@ -353,7 +359,6 @@ class _MapPageState extends State<MapPage> {
           const SizedBox(height: 16),
           ElevatedButton(
             onPressed: () {
-              // Form sayfasına git ve parametre gönder
               Navigator.push(
                 context,
                 MaterialPageRoute(
@@ -364,8 +369,7 @@ class _MapPageState extends State<MapPage> {
                       country: countryController.text,
                       district: districtController.text,
                       street: streetController.text,
-                      address:
-                          '${cityController.text}, ${countryController.text}, ${districtController.text}, ${streetController.text}',
+                      address: '${cityController.text}, ${countryController.text}, ${districtController.text}, ${streetController.text}',
                     ),
                   ),
                 ),
@@ -373,8 +377,7 @@ class _MapPageState extends State<MapPage> {
             },
             style: ElevatedButton.styleFrom(
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(
-                    40), // Burada yuvarlaklık ayarlayabilirsiniz
+                borderRadius: BorderRadius.circular(40),
               ),
             ),
             child: const Text('Continue'),
